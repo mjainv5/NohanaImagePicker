e6a22187579c3c5733597754cd60cf845b129f41
ashokbhakharios <ashokbhakhar@gmail.com>
2019-10-21 15:12:26 +0530

    SPM compiler fixes

--- Tests/NohanaImagePickerTests/NohanaImagePickerTests.swift
+++ Tests/NohanaImagePickerTests/NohanaImagePickerTests.swift
@@ -6,7 +6,7 @@ final class NohanaImagePickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NohanaImagePicker().text, "Hello, World!")
+        //XCTAssertEqual(NohanaImagePicker.text, "Hello, World!")
     }
 
     static var allTests = [
